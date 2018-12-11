b9177b2fa035fbbee2005fd23cbc12894259ca25
carlimartinez <martinezcarli@gmail.com>
2018-12-11 11:13:33 -0700

    add user FK to classrooms

---
+++ db/migrate/20181211181025_build_classroom_user_relationship.rb
@@ -0,0 +1,5 @@
+class BuildClassroomUserRelationship < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :classrooms, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_175047) do
+ActiveRecord::Schema.define(version: 2018_12_11_181025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_12_11_175047) do
   create_table "classrooms", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_classrooms_on_user_id"
   end
 
   create_table "classrooms_profiles", id: false, force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2018_12_11_175047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "classrooms", "users"
 end
