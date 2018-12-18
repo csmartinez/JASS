f2b50e25d5c0310d28b40a54187c312d8d60204e
carlimartinez <martinezcarli@gmail.com>
2018-12-17 19:46:47 -0700

    test sortable table columns

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,6 @@
 class ClassroomsController < ApplicationController
   def index
-    @classrooms = Classroom.all
+    @classrooms = Classroom.order("#{sort_column} #{sort_direction}")
     @profiles = Profile.all
   end
 
@@ -8,5 +8,18 @@ class ClassroomsController < ApplicationController
     @classroom = Classroom.find(params[:id])
     render :show
     @profiles = Profile.all
+  end
+
+private
+  def sortable_columns
+    ["fname", "lname", "ethnicity", "iep", "math", "ela"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "fname"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
