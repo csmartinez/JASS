9406f617c005bed4899eb3ca4176ebd33d6ad1bf
carlimartinez <martinezcarli@gmail.com>
2018-12-17 20:38:35 -0700

    flesh out sorting

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -2,8 +2,8 @@ module ClassroomsHelper
 	def sort_link(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
+    icon = sort_direction == "asc" ? "fas fa-sort-up" : "fas fa-sort-down"
     icon = column == sort_column ? icon : ""
-    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+    link_to "#{title} <i class='#{icon}'></i>".html_safe, {column: column, direction: direction}
   end
 end
