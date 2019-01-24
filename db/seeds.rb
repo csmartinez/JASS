d364616d7e1884eb8cb93bb082220e5ee15be038
JDMitch <jdm568@nau.edu>
2019-01-23 21:50:28 -0700

    Updated Profile Info for Local Testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+profiles = Profile.create([{
+  fname: 'Linda',
+  lname: 'Batala',
+  ethnicity: 'Native American',
+  iep: 'YES',
+  background: '',
+  math: 'Meeting standards'
+},
+{
+  fname: 'Skyker',
+  lname: 'Bell',
+  ethnicity: 'White',
+  iep: 'NO',
+  background: '',
+  math: 'Meeting standards'
+},
+{
+  fname: 'Ryan',
+  lname: 'Lee',
+  ethnicity: 'Native American',
+  iep: 'NO',
+  background: '',
+  math: 'Meeting standards'
+},
+{
+  fname: 'Anastasia',
+  lname: 'Everson',
+  ethnicity: 'White',
+  iep: 'NO',
+  background: '',
+  math: 'Excelling standards'
+},
+{
+  fname: 'Shawna',
+  lname: 'Givens',
+  ethnicity: 'Native American',
+  iep: 'YES',
+  background: '',
+  math: 'Falling far below'
+},
+{
+  fname: 'Elizabeth',
+  lname: 'Holliman',
+  ethnicity: 'Native American',
+  iep: 'Single parents with Mom',
+  background: '',
+  math: 'Falling far below'
+},
+{
+  fname: 'Robert',
+  lname: 'Johnson',
+  ethnicity: 'Native American',
+  iep: 'YES',
+  background: '',
+  math: 'Meeting standards'
+},
+{
+  fname: 'Aaron',
+  lname: 'Kock',
+  ethnicity: 'Native American',
+  iep: 'NO',
+  background: '',
+  math: 'Meeting standards'
+},
+{
+  fname: 'Samantha',
+  lname: 'Lopez',
+  ethnicity: 'Native American',
+  iep: 'NO',
+  background: '',
+  math: 'Meeting standards'
+},
+{
+  fname: 'Chelsey',
+  lname: 'Lovett',
+  ethnicity: 'Native American',
+  iep: 'NO',
+  background: '',
+  math: 'Meeting standards'
+}
+
+])
