19930b3395f6022fdefc028b0914e5dacc36a82f
carlimartinez <martinezcarli@gmail.com>
2019-03-05 19:49:47 -0700

    try user progress uniqueness

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -2,5 +2,5 @@ class Progression < ApplicationRecord
 	belongs_to :user
 	belongs_to :scenario
 
-	validates_uniqueness_of :user_id, :scope => :scenario_id
+	validates_uniqueness_of :complete, :scope => [:user_id, :scenario_id]
 end
