fa9b2e8cfe91a111782143ab748a63bc1c513f13
Karsten Nguyen <khn6@nau.edu>
2019-03-13 19:31:33 -0700

    Cleaned up Search Method

--- app/models/classprofile.rb
+++ app/models/classprofile.rb
@@ -2,15 +2,6 @@ class Classprofile < ApplicationRecord
 	belongs_to :profile
 	belongs_to :classroom
 
-	def self.search(search)
-		if search
-			where(["lower(fname) LIKE ? OR lower(lname) LIKE ? OR lower(ethnicity) LIKE ? OR lower(iep) LIKE ?
-				OR lower(math) LIKE ? OR lower(ela) LIKE ? OR (fname) LIKE ? OR (lname) LIKE ?
-				OR (ethnicity) LIKE ? OR (iep) LIKE ? OR (math) LIKE ? OR (ela) LIKE ?" ,
-				"%#{search}%", 	"%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%",
-				"%#{search}%",  "%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%"])
-			end
-		end
 
 
 end
--- app/models/classroomoffering.rb
+++ app/models/classroomoffering.rb
@@ -3,16 +3,6 @@ class Classroomoffering < ApplicationRecord
 	belongs_to :offering
 	has_many :profilevisibles
 
-	def self.search(search)
-		if search
-			where(["lower(fname) LIKE ? OR lower(lname) LIKE ? OR lower(ethnicity) LIKE ? OR lower(iep) LIKE ?
-				OR lower(math) LIKE ? OR lower(ela) LIKE ? OR (fname) LIKE ? OR (lname) LIKE ?
-				OR (ethnicity) LIKE ? OR (iep) LIKE ? OR (math) LIKE ? OR (ela) LIKE ?" ,
-				"%#{search}%", 	"%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%",
-				"%#{search}%",  "%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%"])
-			end
-		end
-
 
 
 end
