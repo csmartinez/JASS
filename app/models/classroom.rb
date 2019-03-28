183adb850901444c1226f79dd7d74cc69db22d38
Karsten Nguyen <khn6@nau.edu>
2019-03-27 17:37:03 -0700

    Profile and Classroom Search

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,17 +5,5 @@ class Classroom < ApplicationRecord
 	has_many :offerings, through: :classroomofferings
 
 
-	def self.search(search)
-		if search
-			where(["lower(fname) LIKE ? OR lower(lname) LIKE ? OR lower(ethnicity) LIKE ? OR lower(iep) LIKE ? OR lower(math) LIKE ? OR lower(ela) LIKE ?
 
-				OR (fname) LIKE ? OR (lname) LIKE ? OR (ethnicity) LIKE ? OR (iep) LIKE ? OR (math) LIKE ? OR (ela) LIKE ?
-
-				OR upper(fname) LIKE ? OR upper(lname) LIKE ? OR upper(ethnicity) LIKE ? OR upper(iep) LIKE ? OR upper(math) LIKE ? OR upper(ela) LIKE ?" ,
-
-				"%#{search}%", 	"%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%",
-				"%#{search}%",  "%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%",
-				"%#{search}%",  "%#{search}%", 	"%#{search}%", 	"%#{search}%", "%#{search}%", "%#{search}%",])
 	end
-end
-end
