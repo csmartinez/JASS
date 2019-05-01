6a83febf0688118e2f1835ec718066c19a93a557
Ruben <rr779@nau.edu>
2019-05-01 13:19:43 -0700

    Fixed typo on deletion

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,7 +2,7 @@ class Scenario < ApplicationRecord
 	has_many :progressions
 	has_many :users, -> { distinct }, through: :progressions, dependent: :destroy
 
-	has_many :exercisecenarios
+	has_many :exercisescenarios
 	has_many :exercises, through: :exercisescenarios, dependent: :destroy
 
 	mount_uploader :pdf_file, ScenarioUploader
